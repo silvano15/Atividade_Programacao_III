b9e7fa8446776f635c64270d8e536c8db2ff8c80
Silvano Antonio <64760319+silvano15@users.noreply.github.com>
2026-08-19 14:15:38 -0300

    Ajustando o arquivo Main

--- Atividade_Programação_III(Questao 1)/Main.java
+++ Atividade_Programação_III(Questao 1)/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
     Autor meuAutor = new Autor("Walcyr Carrasco");
-    Usuario meuUsuario = new Usuario("Gerson");
-    Livro meuLivro = new Livro("A Volta ao Mundo em 80 Dias", meuAutor);
-    Emprestimo emp = new Emprestimo(meuUsuario, meuLivro);
-    System.out.println("O livro " + emp.getLivro().getTitulo() + " foi emprestado para o usuário " + emp.getUsuario().getNome() + " e o autor do livro é " + emp.getLivro().getAutorLivro().getNome());
+    Usuario meuUsuario = new Usuario("Gerson", 123456789-11, 2);
+    Livro meuLivro = new Livro("A Volta ao Mundo em 80 Dias", true, meuAutor);
+    Emprestimo emp = new Emprestimo(meuLivro, meuUsuario);
+    System.out.println("Você realizou um empréstimo: " + emp.getDadosUsuario().getNome() + ", " + emp.getLivroEmprestado().getTitulo());
 }
 }
