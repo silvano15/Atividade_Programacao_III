31b59f2f011f25fa2899cfcd111f3730a9d220b2
Silvano Antonio <64760319+silvano15@users.noreply.github.com>
2026-08-19 14:27:56 -0300

    Ajustando arquivo Main

--- Atividade_Programação_III(Questao 1)/Main.java
+++ Atividade_Programação_III(Questao 1)/Main.java
@@ -5,6 +5,6 @@ public class Main {
     Usuario meuUsuario = new Usuario("Gerson", 123456789-11, 2);
     Livro meuLivro = new Livro("A Volta ao Mundo em 80 Dias", true, meuAutor);
     Emprestimo emp = new Emprestimo(meuLivro, meuUsuario);
-    System.out.println("Você realizou um empréstimo: " + emp.getDadosUsuario().getNome() + ", " + emp.getLivroEmprestado().getTitulo());
+    System.out.println("Você realizou um empréstimo: " + emp.getDadosUsuario().getNome() + ", " + emp.getLivroEmprestado().getTitulo() + ", do autor " + emp.getLivroEmprestado().getAutorLivro().getNome());
 }
 }
