06ccf81930864ee8a4f6aa2b0be62e919a812c67
Gabriel Cavalcante dos Santos Lins <202517640044@unifacol.edu.br>
2026-08-20 13:42:58 -0300

    2º critério alternativo
    Removed comments explaining the comparator's functionality.

--- Atividade_Programacao_III(Questao 8)/ComparatorSalarioDecrescente.java
+++ Atividade_Programacao_III(Questao 8)/ComparatorSalarioDecrescente.java
@@ -1,7 +1,5 @@
  import java.util.Comparator;
 
-// Ordena por salário do maior para o menor. Repare que é só
-// inverter a ordem dos argumentos em relação ao comparator anterior.
 public class ComparatorSalarioDecrescente implements Comparator<Funcionario> {
     @Override
     public int compare(Funcionario f1, Funcionario f2) {
